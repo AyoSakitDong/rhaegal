4c130ae42c4b5c84b188d37f9e402898a5125449
raisoturu <anvaqta@student.telkomuniversity.ac.id>
2019-12-04 21:31:21 +0700

    fix email validation

---
+++ functions/users/order.js
@@ -0,0 +1,3 @@
+const { firebase, db } = require("../utils/firebaseConfig");
+const { validateLoginData } = require("../utils/validator");
+export const addToCart = (req, res) => {};
--- functions/utils/validator.js
+++ functions/utils/validator.js
@@ -1,8 +1,7 @@
 const isEmail = email => {
-  const regEx = new RegExp(
-    '/^(([^<>()[]\\.,;:s@"]+(.[^<>()[]\\.,;:s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/'
-  );
-  if (email.match(regEx)) return true;
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; // eslint-disable-line
+  console.log(regEx.test(email));
+  if (regEx.test(String(email).toLowerCase())) return true;
   else return false;
 };
 const isEmpty = inp => {
